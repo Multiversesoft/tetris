814ad26f6996427d2b607310829d50c9b77c304e
praharshbhatt <prahu.bhatt@gmail.com>
2019-06-18 19:20:47 +0530

    Changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'tetris',
+      title: 'Falling Blocks',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         S.delegate,
